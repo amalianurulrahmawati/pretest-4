ee75533eb2f8dda7aded285afc01468d0be25c7c
amalianurulrahmawati <92409385+amalianurulrahmawati@users.noreply.github.com>
2021-11-20 19:56:17 +0700

    Add files via upload

---
+++ 2117051008_Amalia Nurul Rahmawati_Pretest4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	int a,b;
+	a=0;
+	b=36;
+	
+	cout<<"Bilangan Awal :"<< a<<endl;
+	cout<<"Bilangan Akhir :"<< b<<endl;
+	
+	//input
+	int batas,loop=0;
+	
+	cout<<"----------------------------------------\n";
+	cout<<"Masukan Bilangan Yang Berupa Kelipatan 3\n";
+	cout<<"Masukan Batas Deret :";
+	cin>>batas;
+	cout<<endl;
+	cout<<"   ";
+	while
+	(loop<=batas)
+	{
+		cout<<loop<<" ";
+		loop+=3;
+		cout<<endl;
+	}
+	
+	return 0;
+
+		
+	
+}
